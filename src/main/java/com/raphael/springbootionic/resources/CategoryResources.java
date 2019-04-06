15381fde9cd53fbb93840b16755961a258e91fc1
Raphael Gomes <raphaelg9@gmail.com>
2019-04-05 23:34:15 -0300

    Testing the first domain class category

--- src/main/java/com/raphael/springbootionic/resources/CategoryResources.java
+++ src/main/java/com/raphael/springbootionic/resources/CategoryResources.java
@@ -1,16 +1,29 @@
 package com.raphael.springbootionic.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raphael.springbootionic.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST API is working";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(1, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
