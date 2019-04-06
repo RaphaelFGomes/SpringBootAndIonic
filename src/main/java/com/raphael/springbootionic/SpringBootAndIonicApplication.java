6673a8bd8d43b8a6b95b3bf5c0e10072c8654985
Raphael Gomes <raphaelg9@gmail.com>
2019-04-05 23:05:13 -0300

    Project has been created

---
+++ src/main/java/com/raphael/springbootionic/SpringBootAndIonicApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.springbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndIonicApplication.class, args);
+	}
+
+}
