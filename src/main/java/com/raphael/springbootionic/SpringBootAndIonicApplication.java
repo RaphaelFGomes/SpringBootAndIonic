bf8a22d47ab3a5fcff63647b0217f8c00e872918
Raphael Gomes <raphaelg9@gmail.com>
2019-04-06 16:05:47 -0300

    Product and association many to many

--- src/main/java/com/raphael/springbootionic/SpringBootAndIonicApplication.java
+++ src/main/java/com/raphael/springbootionic/SpringBootAndIonicApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.raphael.springbootionic.domain.Category;
+import com.raphael.springbootionic.domain.Product;
 import com.raphael.springbootionic.repositories.CategoryRepository;
+import com.raphael.springbootionic.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringBootAndIonicApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAndIonicApplication.class, args);
@@ -25,7 +30,19 @@ public class SpringBootAndIonicApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "Computing");
 		Category cat2 = new Category(null, "Office");
 		
+		Product p1 = new Product(null, "Computer", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
