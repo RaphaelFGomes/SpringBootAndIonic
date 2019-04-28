80ee923cdfb79045c2415da63e34a446c492972e
Raphael Gomes <raphaelg9@gmail.com>
2019-04-28 15:30:57 -0300

    Adding new category with POST

--- src/main/java/com/raphael/springbootionic/services/CategoryService.java
+++ src/main/java/com/raphael/springbootionic/services/CategoryService.java
@@ -20,5 +20,10 @@ public class CategoryService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
 		}
+	
+	public Category insertCategory(Category obj) {
+		obj.setId(null);
+		return repo.save(obj);
+		}
 
 }
